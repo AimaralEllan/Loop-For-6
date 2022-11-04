3e796152dd100d60f273383ffc8e86e2f13f7f9a
Mac <mac@Macs-MacBook-Pro.local>
2022-11-04 20:48:16 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Loop For  Циклдын уландысы
+        //For менен While окшош болгону For менен код кыскарат
+
+        /*int a=1;
+        while (a<10){
+            System.out.println(a+ " Hello Java");
+            a++;
+        }*/
+         /*for (int i=0; i<=10; i++){ //Внутри() все условия пишется
+             System.out.println(i+ " Hello Aimaral");
+         }*/
+
+        Scanner input=new Scanner(System.in);
+
+        /*while (true){
+            System.out.println("San ber");
+            int a=input.nextInt();
+            if (a<0){
+                break;
+            }
+        }
+        System.out.println("The End");*/
+
+        /*for(;true;){
+            System.out.println("San jaz");
+            int a=input.nextInt();
+            if(a<0){
+                break;
+            }
+        }
+        System.out.println("The En6td");*/
+        /*while (true) {
+            System.out.println("San beriniz");
+            int a = input.nextInt();
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("%d * %d = %d\n", a, i, a * i);
+            }
+        }*/
+
+        /*int sum=0;
+        for (int i=1;i<=300;i++){
+            if (i%3==0 && i%4==0 && i%5==0){
+                System.out.println(i);
+               sum=sum+i;
+            }
+        }
+        System.out.println("3ко 4ко жана 5ке болунгон сандардын суммасы: "+sum);*/
+
+        /*for (int i=1;i<=10;i++){
+            System.out.println(Math.pow(2,i));
+        }*/
+        
+        }
+    }
